1737c583d683ed990fddcbd2dc29d78f33097270
ycs-wikim <unangel@yuhan.ac.kr>
2026-06-15 15:55:18 +0900

    add codes

---
+++ 0615/0615/0615.cpp
@@ -0,0 +1,85 @@
+﻿// 0615.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+using namespace std;
+#include <windows.h>
+
+
+/// 함수의 오버로딩 : 인수/매개변수의 차이
+/*
+int add(int a, int b){}
+float add(float a, float b) {}
+*/
+
+
+/// 템플릿 프로그래밍 : 형식을 만드는 프로그래밍 방식
+template <typename T>
+T add(T a, T b)
+{
+    T c;
+    T d;
+    c = a + b;
+    d = c * 10;
+    std::cout << d << std::endl;
+    return c;
+}
+
+/// namespace는 논리적인 코드 묶음
+namespace ASDF
+{
+    int g_x;
+
+    void asdf()
+    {
+        printf("asdf\n");
+    }
+}
+
+namespace ASDF
+{
+    class CTest
+    {
+    public:
+        int m_x;
+    };
+    void bbb()
+    {
+        printf("bbb\n");
+    }
+}
+
+
+/// 스레드 함수 : 형식이 고정
+DWORD WINAPI pig(LPVOID param)
+{
+    for (int i = 0; i < 100; i++)
+    {
+        printf("i = %d\n", i);
+        Sleep(100);
+    }
+
+    ExitThread(0);
+    return 0;
+}
+
+
+int main()
+{
+    /// 스레드 생성
+    CreateThread(NULL, 0, pig, NULL, 0, NULL);
+    /// 20초간 대기
+    Sleep(20000);
+
+    using namespace ASDF;
+    asdf();
+    bbb();
+    ASDF::asdf();
+    CTest a;
+
+    std::cout << add(355, 877) << std::endl;
+    //std::cout << add(355, 877.9) << std::endl;
+    std::cout << add('3', '8') << std::endl;
+    std::cout << add(355.87, 8.77) << std::endl;
+    std::cout << "Hello World!\n";
+}
---
+++ 0615_game/0615_game/0615_game.cpp
@@ -0,0 +1,36 @@
+﻿// 0615_game.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+using namespace std;
+/// 랜덤 함수 사용을 위한 헤더
+#include <stdlib.h>
+/// 시간 획득 함수 - 랜덤 시드 값 변경
+#include <time.h>
+
+
+int main()
+{
+    const char* game[3][3] = {
+        "비겼다", "졌다", "이겼다",
+        "이겼다", "비겼다", "졌다",
+        "졌다", "이겼다", "비겼다"
+    };
+
+
+    /// 시드 값 변경
+    srand(time(NULL));
+
+    int user;
+    int com;
+    /// 사용자 입력
+    cout << "가위(0), 바위(1), 보(2) : ";
+    cin >> user;
+
+
+    /// 컴퓨터 값 : 랜덤 값 획득
+    com = rand() % 3;
+    printf("com : %d - user - %d : %s\n", com, user, game[user][com]);
+
+    std::cout << "Hello World!\n";
+}
